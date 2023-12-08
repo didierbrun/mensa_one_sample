2a085aaf612d0e3905ca7a7fc0f3efeabda41285
Didier Brun <db@brundidier.com>
2023-12-08 21:46:03 +0100

    Add GraphQL + Cloudinary

---
+++ build/7365.187c44e6.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksample=self.webpackChunksample||[]).push([[7365],{77365:(s,e,d)=>{d.r(e),d.d(e,{default:()=>n});const n={"plugin.description.long":"Adds GraphQL endpoint with default API methods.","plugin.description.short":"Adds GraphQL endpoint with default API methods.","plugin.name":"GraphQL"}}}]);
---
+++ build/8762.5fb031ba.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksample=self.webpackChunksample||[]).push([[8762],{58762:(d,n,e)=>{e.r(n),e.d(n,{default:()=>p});const p={"plugin.description.long":"Dodaje GraphQL endpoint z domy\u015Blnymi metodami API.","plugin.description.short":"Dodaje GraphQL endpoint z domy\u015Blnymi metodami API.","plugin.name":"GraphQL"}}}]);
